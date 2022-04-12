b3ea20577ebb88d2429c0303064c1d6ca2f08c9f
Ganesh <sawganeshkumar15@gmail.com>
2022-04-12 12:20:26 +0530

    setting configuration v1.0

---
+++ src/main/java/com/code4you/cs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.code4you.cs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConfigurationServerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/code4you/cs/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4you.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
